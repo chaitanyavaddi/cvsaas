c633631fee6f5a48f02d6bb2641dc3d96d0cb26b
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-02-11 23:07:03 +0530

    (fix) fixing node npm tailwind

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,26 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../templates/**/*.{html, js}"],
+  content: [],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
