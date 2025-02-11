f096d488f82e27f0534b43a370b8e07cdd894eb6
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-02-11 23:47:29 +0530

    (feature) integrated node tailwind build

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "../templates/**/*.{html, js}",
+    'node_modules/preline/dist/*.js',
+    '../static/js/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('preline/plugin'),
+  ],
 }
 
